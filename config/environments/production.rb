52623e5cd154a57e1b765bfb783461f5b014b76a
showwin <showwin_kmc@yahoo.co.jp>
2016-06-22 18:45:09 +0900

    [fix] deploy setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.log_formatter = ::Logger::Formatter.new
+  config.logger = Logger.new('log/production.log', 'daily')
 end
